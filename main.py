9782a1b1d045808ee7c80cc108f0c26e8bbe97ee
Thai Doan <thaidv96@gmail.com>
2021-02-24 16:39:18 +0700

    Initialize with ingest starting data

--- main.py
+++ main.py
@@ -20,9 +20,9 @@ def start_data():
     df.to_sql('financial', con = connection, schema='source',if_exists='replace')
     ## validate ingesting data 
     sql = "Select count(1) as num_records from source.financial"
-    check_result = pd.read_sql(sql, con = connection)
+    check_result = pd.read_sql(sql, con = connection)['num_records'].values[0]
     print("Input Data Size: ", input_data_size)
-    print("Ingested Data Size:", check_result['num_records'].values[0])
+    print("Ingested Data Size:", check_result)
     with open("ingest_start_data.log",'a+') as f:
         f.write(f"{datetime.now()},{input_data_size},{check_result}\n")
     print("SUCCESS")
