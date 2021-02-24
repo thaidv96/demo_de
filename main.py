4690d1318acfc868b9ed9c6b0a01af165d1a1ba7
Thai Doan <thaidv96@gmail.com>
2021-02-24 19:39:17 +0700

    Initialize with ingest starting data

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ from datetime import datetime
 
 parser = ArgumentParser()
 
-parser.add_argument("--job_type", type=str, default='start')
+parser.add_argument("--job_type", type=str, default='etl', required=False)
 parser.add_argument("--root_password", type=str, required=True)
 
 args = parser.parse_args()
-engine = create_engine(f'mysql+pymysql://root:{args.root_password}@localhost/source')
-connection = engine.connect()
+engine = create_engine(f'mysql+pymysql://root:{args.root_password}@localhost/stagging')
+stagging_connection = engine.connect()
 target_engine = create_engine(f'mysql+pymysql://root:{args.root_password}@localhost/target')
 target_connection = target_engine.connect()
 
@@ -19,39 +19,39 @@ def verify_data_ingestion():
 
 
 def start_data():
-    print("START INGESTING DATA TO SOURCE...")
+    print("START INGESTING DATA TO STAGGING...")
     df = pd.read_excel('./datasets/Financial Sample.xlsx')
     df.columns = [i.lower().replace(" ","_") for i in df.columns]
     input_data_size = df.shape[0]
-    df.to_sql('financial', con = connection, schema='source',if_exists='replace')
-    ## validate ingesting data 
-    sql = "Select count(1) as num_records from source.financial"
-    check_result = pd.read_sql(sql, con = connection)['num_records'].values[0]
-    print("Input Data Size: ", input_data_size)
-    print("Ingested Data Size:", check_result)
-    with open("ingest_start_data.log",'a+') as f:
-        f.write(f"{datetime.now()},{input_data_size},{check_result}\n")
-    print("SUCCESS")
+    df.to_sql('financial', con = stagging_connection, schema='stagging',if_exists='replace')
+    print("SUCCESS LOAD DATA TO STAGGING AREA")
 
-def gen_simple_dim_select_sql(*fields, table='financial'):
+def gen_simple_dim_select_sql(*fields, table='stagging_financial'):
     return f"""Select distinct {','.join(fields)} from {table}"""
 
 
-def etl_dim():
-    dim_product_sql = gen_simple_dim_select_sql('product', 'manufacturing_price')
-    dim_product_df = pd.read_sql(dim_product_sql, con=connection)
-    dim_product_df.reset_index()
-    dim_product_df.rename(columns={"index":"id"})
-    dim_product_df.to_sql('dim_product',con=connection, schema='target', if_exists='replace', index=False)
+def etl_dim(*cols,table_name=None,connection):
+    print(f"GENERATING {table_name} IN STAGGING AREA...")
+    sql = gen_simple_dim_select_sql(*cols)
+    dim_df = pd.read_sql(sql, con=connection)
+    dim_df.reset_index(inplace=True)
+    dim_df.rename(columns={"index":"id"}, inplace=True)
+    dim_df.to_sql(table_name,con=connection, schema='stagging', if_exists='replace', index=False)
+    print("DONE")
+
+
+def etl_dims():
+    start_data()
+    etl_dim('product','price','discount_band', table_name='dim_product', connection=stagging_connection)
+    etl_dim('segment', table_name='dim_segment', connection=stagging_connection)
+    etl_dim('country', table_name='dim_country', connection=stagging_connection)
 
 
 def etl():
-    etl_dim()
+
+    etl_dims()
 
 if __name__ == '__main__':
-    if args.job_type == 'start':
-        start_data()
-
     if args.job_type == 'etl':
         etl()    
 
